9639d8d9e010634e598d8780e8ca5c8348f18312
Michaël Dierick <michael.dierick@redpencil.io>
2022-12-21 10:43:36 +0100

    query performance improvement

--- services/content-unification/sparql_util.py
+++ services/content-unification/sparql_util.py
@@ -20,8 +20,10 @@ BATCH_SIZE = 100
 # adapted from https://github.com/RDFLib/rdflib/issues/1704
 def serialize_graph_to_sparql(g, graph_name: str):
     for triples_batch in batched(g.triples((None, None, None)), BATCH_SIZE):
-        updatequery = "\n".join([f"PREFIX {prefix}: {ns.n3()}" for prefix, ns in g.namespaces()])
-        updatequery += f"\nINSERT DATA {{\n\tGRAPH {sparql_escape_uri(graph_name)} {{\n"
+        # updatequery = "\n".join([f"PREFIX {prefix}: {ns.n3()}" for prefix, ns in g.namespaces()])
+        # Dropping prefixes boosts performance. Since the '.n3()'-method produces ntriples compliant triples,
+        # dropping the prefixes yields an ntriples only query, which is significantly faster
+        updatequery = f"\nINSERT DATA {{\n\tGRAPH {sparql_escape_uri(graph_name)} {{\n"
         updatequery += " .\n".join([f"\t\t{s.n3()} {p.n3()} {o.n3()}" for (s, p, o) in triples_batch])
         updatequery += f" . \n\t }}\n}}\n"
         yield updatequery
