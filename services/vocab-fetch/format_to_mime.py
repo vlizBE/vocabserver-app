435c2282d27a659b0cff42203526a2f93b91ad88
Kobe Mertens <kobe.mertens@hotmail.com>
2022-12-28 17:18:15 +0100

    vocab-fetch: add support for jsonld

--- services/vocab-fetch/format_to_mime.py
+++ services/vocab-fetch/format_to_mime.py
@@ -4,4 +4,5 @@ FORMAT_TO_MIME_EXT = {
     "http://www.w3.org/ns/formats/RDF_XML": ("application/rdf+xml", "rdf"),
     # "http://www.w3.org/ns/formats/RDFa":
     "http://www.w3.org/ns/formats/Turtle": ("text/turtle", "ttl"),
+    "https://www.w3.org/ns/formats/data/JSON-LD": ("application/ld+json", "jsonld")
 }
